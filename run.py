b95357f29e09373335cf47e82af804bfc674c51b
db0 <mail@dbzer0.com>
2024-02-25 13:57:28 +0100

    feat: Add docker creation workflows

--- run.py
+++ run.py
@@ -4,9 +4,11 @@ from loguru import logger
 
 from threativore import threativore
 from threativore.flask import APP
+from threativore.consts import THREATIVORE_VERSION
 
 if __name__ == "__main__":
     with APP.app_context():
+        logger.init(f"Threadivore {THREATIVORE_VERSION}", status="Starting")
         while True:
             try:
                 threativore.check_pms()
@@ -17,3 +19,4 @@ if __name__ == "__main__":
             except Exception as err:
                 logger.warning(f"Exception during loop: {err}. Will continue after sleep...")
                 time.sleep(1)
+    logger.init_ok(f"Threadivore {THREATIVORE_VERSION}", status="Exited")
