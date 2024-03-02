155a87f524a82fe609163cc32651c675995f9f87
thekingoflorda <64592718+thekingoflorda@users.noreply.github.com>
2024-03-02 01:30:01 +0100

    Add slack webhook (#11)

--- threativore/webhooks.py
+++ threativore/webhooks.py
@@ -1,9 +1,16 @@
 import requests
 import os
 
+from loguru import logger
+
 def webhook_parser(message: str):
     if os.getenv("DISCORDWEBHOOK") != "None":
-        post_discord_webhook(url=os.getenv("DISCORDWEBHOOK"), message=message,username=None, avatar_url=None)
+        if not post_discord_webhook(url=os.getenv("DISCORDWEBHOOK"), message=message,username=None, avatar_url=None):
+            logger.debug("Discord webhook failed")
+            
+    if os.getenv("SLACKWEBHOOK") != "None":
+        if post_slack_webhook(url=os.getenv("SLACKWEBHOOK"), message=message):
+            logger.debug("Slack webhook failed")
 
 def post_discord_webhook(url: str, message:str, username:str, avatar_url:str) -> bool:
     if username == None:
@@ -20,4 +27,14 @@ def post_discord_webhook(url: str, message:str, username:str, avatar_url:str) ->
         return True
     else:
         return False
-    
+
+def post_slack_webhook(url: str, message: str) -> bool:
+    payload = {
+        "text": message
+    }
+
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
