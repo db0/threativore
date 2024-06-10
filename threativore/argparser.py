deb570bdd069525ab02fcc91d6a90c2ebdac9e75
db0 <mail@dbzer0.com>
2024-06-11 01:16:51 +0200

    feat: THREATIVORE_VERBOSITY

--- threativore/argparser.py
+++ threativore/argparser.py
@@ -1,34 +1,20 @@
+import os
 import argparse
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(
-    "-i",
-    "--insecure",
-    action="store_true",
-    help="If set, will use http instead of https (useful for testing)",
-)
-arg_parser.add_argument(
     "-v",
     "--verbosity",
     action="count",
-    default=0,
+    default=int(os.getenv("THREATIVORE_VERBOSITY",0)),
     help="The default logging level is ERROR or higher. This value increases the amount of logging seen in your screen",
 )
 arg_parser.add_argument(
     "-q",
     "--quiet",
     action="count",
-    default=0,
+    default=int(os.getenv("THREATIVORE_QUIET",0)),
     help="The default logging level is ERROR or higher. This value decreases the amount of logging seen in your screen",
-)
-arg_parser.add_argument(
-    "-p",
-    "--port",
-    action="store",
-    default=7001,
-    required=False,
-    type=int,
-    help="Provide a different port to start with",
 )
 arg_parser.add_argument(
     "--gc_days",
